67c12340ca2c2ce42eed0caefaa7a85f3db37d6a
Ravi Eragapati <e.ravikanth@live.com>
2025-08-17 21:55:43 -0400

    2 setup vectorstore (#3)
    * added vectorStore and Custom prompt

--- src/main/java/com/eragapati/springai/llama/controller/ChatController.java
+++ src/main/java/com/eragapati/springai/llama/controller/ChatController.java
@@ -1,7 +1,7 @@
 package com.eragapati.springai.llama.controller;
 
+import com.eragapati.springai.llama.service.LlamaPromptService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,14 +13,35 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class ChatController {
 
-    private final ChatClient chatClient;
+    private final LlamaPromptService llamaPromptService;
 
+    /**
+     * This endpoint doesnot uses RAG nor the Prompt was built to add context so the outcome will not be streamlined.
+     * @param token - the original token/question
+     * @param id - Will be used in future to track chat history for individual chat id
+     * @return - Map that contains response
+     */
     @GetMapping("/chat/{id}/ask")
-    public Map<String,String> ask(
+    public Map<String, String> ask(
             @RequestParam(value = "token", defaultValue = "Tell me a joke")
             String token,
             @PathVariable
             long id) {
-        return Map.of("response",this.chatClient.prompt().user(token).call().content());
+        return Map.of("response", llamaPromptService.callAIModel(token));
+    }
+
+    /**
+     * This endpoint uses RAG and also a custom Prompt is built to add context and attach documents retrieved from VectorStore that have close similarity to the question.
+     * @param token - the original token/question
+     * @param id - Will be used in future to track chat history for individual chat id
+     * @return - Map that contains response
+     */
+    @GetMapping("/chat/{id}/askUsingRAG")
+    public Map<String, String> askUsingRAG(
+            @RequestParam(value = "token", defaultValue = "Tell me a joke")
+            String token,
+            @PathVariable
+            long id) {
+        return Map.of("response", llamaPromptService.callAIModelUsingRAG(token));
     }
 }
