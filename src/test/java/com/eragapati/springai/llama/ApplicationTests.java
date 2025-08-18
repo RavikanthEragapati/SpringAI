04d417a08818732d3bfabdb4a6c7c89eb6115362
ravieragapati <e.ravikanth@live.com>
2025-08-17 20:43:09 -0400

    Init commit

---
+++ src/test/java/com/eragapati/springai/llama/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eragapati.springai.llama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
